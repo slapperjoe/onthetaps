f6fa662853ef0a7cf6cc08676aed5195afa20570
slapperjoe <markr@live.com>
2022-11-21 08:01:59 +1100

    Additional auth

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
+using Client.Authentication;
 using Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -8,8 +10,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<BrowserService>();
+builder.Services.AddMudServices()
+						.AddAuthorizationCore()
+						.AddScoped<AuthenticationStateProvider, StaticWebAppsAuthenticationStateProvider>()
+						.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) })
+						.AddScoped<BrowserService>();
 
 await builder.Build().RunAsync();
