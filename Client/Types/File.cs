a4b77d46a5effe970630180d5a28b47cb92bd9b5
slapperjoe <markr@live.com>
2022-11-21 20:38:28 +1100

    refactoring and colour cleanup

---
+++ Client/Types/File.cs
@@ -0,0 +1,7 @@
+namespace Types
+{
+	public class File
+	{		
+		public string? Name { get; set; }
+	}
+}
---
+++ Client/Types/UploadResult.cs
@@ -0,0 +1,10 @@
+namespace Types
+{
+	public class UploadResult
+	{
+		public bool Uploaded { get; set; }
+		public string? FileName { get; set; }
+		public string? StoredFileName { get; set; }
+		public int ErrorCode { get; set; }
+	}
+}
