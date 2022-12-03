3efcd60399cf199f2e20b60e860ce95efd72f612
slapperjoe <markr@live.com>
2022-12-03 13:34:02 +1100

    Github login.  Pull out infiv taps.

---
+++ Client/Types/FileItemDTO.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTheTaps.Client.Types
+{
+	public class FileItemDTO
+	{
+		public FileItemDTO(int tap, InputFileChangeEventArgs file, ElementReference loadBox)
+		{
+			tapNumber = tap;
+			this.file = file;
+			this.loadBox = loadBox;
+		}
+		public int tapNumber { get; set; }
+		public InputFileChangeEventArgs file { get; set; }
+		public ElementReference loadBox { get; set; }
+	}
+}
