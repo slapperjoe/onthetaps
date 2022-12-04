e5a83f50a12058ad78f9f7c0dbe18555f34eab77
slapperjoe <markr@live.com>
2022-12-04 23:04:10 +1100

    Additional refactoring for saving changes

---
+++ Client/Types/BeerItemDTO.cs
@@ -0,0 +1,24 @@
+﻿using BigBeerData.Shared.DTOs;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTheTaps.Client.Types
+{
+	public class BeerItemDTO
+	{
+		public BeerItemDTO(int tap, BeerDTO beer, ElementReference loadBox)
+		{
+			tapNumber = tap;
+			this.beer = beer;
+			this.loadBox = loadBox;
+		}
+		public int tapNumber { get; set; }
+		public BeerDTO beer { get; set; }
+		public ElementReference loadBox { get; set; }
+	}
+}
--- Client/Types/FileItemDTO.cs
+++ Client/Types/FileItemDTO.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace OnTheTaps.Client.Types
 {
 	public class FileItemDTO
 	{
+		const int maxFileSize = 10240000;
+
 		public FileItemDTO(int tap, InputFileChangeEventArgs file, ElementReference loadBox)
 		{
 			tapNumber = tap;
@@ -19,5 +22,14 @@ namespace OnTheTaps.Client.Types
 		public int tapNumber { get; set; }
 		public InputFileChangeEventArgs file { get; set; }
 		public ElementReference loadBox { get; set; }
+
+		public string fileName => tapNumber + file.File.Name.Substring(file.File.Name.LastIndexOf('.'));
+
+		public StreamContent GetFileStream()
+		{
+			var fileContent = new StreamContent(file.File.OpenReadStream(maxFileSize));
+			fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.File.ContentType);
+			return fileContent;
+		}
 	}
 }
